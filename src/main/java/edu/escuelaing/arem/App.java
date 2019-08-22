83b38583cbe6f1d57295bd97960eb72abbf608ff
martin <andres.cantor@payulatam.com>
2019-08-21 19:23:18 -0500

    Spark first implementation

--- src/main/java/edu/escuelaing/arem/App.java
+++ src/main/java/edu/escuelaing/arem/App.java
@@ -1,6 +1,7 @@
 package edu.escuelaing.arem;
-
 import edu.escuelaing.arem.model.calculatorKitModel.calculatorKit;
+
+import static spark.Spark.*;
 
 /**
  * This is the main class to execute the PSP0 Program
@@ -10,21 +11,16 @@ import edu.escuelaing.arem.model.calculatorKitModel.calculatorKit;
 public class App {
 
     public static void main( String[] args ) {
+        final calculatorKit calculatorKit = new calculatorKit("./src/main/resources/columna1.txt");
+        port(getPort());
+        get("/mean", (req, res) -> calculatorKit.calculateMean());
+    }
 
-        calculatorKit calculatorKitA = new calculatorKit("./src/main/resources/columna1.txt");
-        calculatorKit calculatorKitB = new calculatorKit("./src/main/resources/columna2.txt");
-        System.out.println("Welcome user !!\n" +
-                "This is the calculator kit\n" +
-                "In this version you can use this for calculate the mean and the standard deviation \n" +
-                "of a set of a n real numbers that are in the files that are in the project resources \n" +
-                "in the first file named 'columna1.txt' are the column data 'Estimate Proxy Size' and \n" +
-                "in the second file named 'columna2.txt' are the column data 'Development Hours'. \n" +
-                "Using the linked list implementation the results are: \n" +
-                "Result 1: column Estimate Proxy Size \n" +  calculatorKitA.getLinkedList() +"\n" +
-                "Mean: " + String.format("%.2f", calculatorKitA.calculateMean()) +"\n" + "Standard deviation: " +  String.format("%.2f",calculatorKitA.calculateStdDeviation()) +
-                "\n" + "Result 2: column Development Hours \n" + calculatorKitB.getLinkedList() +"\n" +
-                "Mean: " +  String.format("%.2f",calculatorKitB.calculateMean()) +"\n" + "Standard deviation: " +  String.format("%.2f",calculatorKitB.calculateStdDeviation()));
-
+    private static int getPort(){
+        if(System.getenv("PORT") != null){
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
     }
 
 }
